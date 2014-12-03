1951935c66ac0744cf671deefe980c01ad4ffa17
jfox <atljeremy@me.com>
2014-12-03 09:11:38 -0500

    Updating README

--- JFTransporter/Service/Operation/JFTransportableOperation.h
+++ JFTransporter/Service/Operation/JFTransportableOperation.h
@@ -8,6 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+#ifndef NS_DESIGNATED_INITIALIZER
+#if __has_attribute(objc_designated_initializer)
+#define NS_DESIGNATED_INITIALIZER __attribute__((objc_designated_initializer))
+#else
+#define NS_DESIGNATED_INITIALIZER
+#endif
+#endif
+
 @protocol JFTransportable;
 @class JFTransportableOperation;
 
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ JFTransporter
 =============
 
 An Objective-C Networking abstraction to help make stepping up a service layer in and iOS app much easier and quicker
+
+###Build/Tests Status:
+![build status](https://travis-ci.org/atljeremy/JFTransporter.svg?branch=master)
